bc5f5fa18c492ec16bcae8d4e0c898c00a1920b5
michellecaner <michellecaner@gmail.com>
2021-11-04 22:42:55 -0500

    loop play part 1

--- scripts/index.js
+++ scripts/index.js
@@ -39,7 +39,34 @@ const wigs = [
   }
 ]
 
-for(let i=0; i < wigs.length; i++) {
-  console.log(wigs[i].name)
-}
+// This loops through each item & shows the true/false value of 'human hair'
 
+// for(let i=0; i < wigs.length; i++) {
+//   console.log(wigs[i].humanHair)
+// };
+
+
+// This makes the color of the 3rd wig show up 4 times
+
+// wigs.forEach(wig => console.log(wigs[2].color))
+
+
+// This makes the name of the 3rd wig show up 4 times
+
+// for(let i=0; i < wigs.length; i++) {
+//   console.log(wigs[i].name)
+// };
+
+
+// This makes the each object show up in a sentence featuring the name, brand and cost.
+
+// for(let i=0; i < wigs.length; i++) {
+//   console.log(`My favorite wig is ${wigs[i].name} by ${wigs[i].brand} and it costs $${wigs[i].price}.`)
+// };
+
+
+// This lists all the objects in the array
+
+// for(let i=0; i < wigs.length; i++) {
+//   console.log(wigs[i])
+// };
